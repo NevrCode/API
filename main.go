d6e073db3aa3f0770e3bf2aa3f0aa822eb0b8561
NevrCode <kevin12keval@gmail.com>
2024-01-24 13:09:40 +0700

    delete header and put directly to main

--- main.go
+++ main.go
@@ -13,12 +13,23 @@ import (
 func init() {
 	initializer.LoadEnvVariables()
 	initializer.ConnectToDB()
-	initializer.HeaderMiddleware()
 }
 
 func main() {
 
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000") // Ganti dengan origin aplikasi klien Anda
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
