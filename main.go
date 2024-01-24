6464dad3addf6476c01d83740304b3570e2621ab
NevrCode <kevin12keval@gmail.com>
2024-01-24 10:40:44 +0700

    initial app

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"API/controller"
+	"API/initializer"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializer.LoadEnvVariables()
+	initializer.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/showUser", controller.ShowUser)
+	r.GET("/showToko", controller.ShowToko)
+	r.Run()
+}
+
+// func main() {
+// http.HandleFunc("/", controller.Index)
+// fmt.Print("jalam mas")
+// http.ListenAndServe(":8080", nil)
+// }
